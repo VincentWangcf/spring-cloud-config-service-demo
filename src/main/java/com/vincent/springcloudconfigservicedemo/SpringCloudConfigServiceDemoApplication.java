71ea3756c199af975f5923a027fecf494ca0ea35
vincent <445047477@qq.com>
2020-03-14 16:40:21 +0800

    添加

--- src/main/java/com/vincent/springcloudconfigservicedemo/SpringCloudConfigServiceDemoApplication.java
+++ src/main/java/com/vincent/springcloudconfigservicedemo/SpringCloudConfigServiceDemoApplication.java
@@ -2,14 +2,16 @@ package com.vincent.springcloudconfigservicedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServiceDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServiceDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServiceDemoApplication.class, args);
+    }
 
 }
